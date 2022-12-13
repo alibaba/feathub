39667f510b8a422fb9518a0c1b1623f96b6cedc2
JiangXin <jiangxin.jiang@alibaba-inc.com>
2022-12-13 16:44:11 +0800

    Raises exception when sinking csv files to s3 using FlinkProcessor
    This closes #51.

---
+++ python/feathub/processors/flink/table_builder/tests/test_filesystem_utils.py
@@ -0,0 +1,29 @@
+#  Copyright 2022 The Feathub Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from feathub.common.exceptions import FeathubException
+from feathub.feature_tables.sinks.file_system_sink import FileSystemSink
+from feathub.processors.flink.table_builder.source_sink_utils import insert_into_sink
+from feathub.processors.flink.table_builder.tests.table_builder_test_base import (
+    FlinkTableBuilderTestBase,
+)
+
+
+class SinkUtilTest(FlinkTableBuilderTestBase):
+    def test_nsupported_file_format(self):
+        sink = FileSystemSink("s3://dummy-bucket/path", "csv")
+        table = self.t_env.from_elements([(1,)])
+        with self.assertRaisesRegex(
+            FeathubException, "Cannot sink files in CSV format to s3"
+        ):
+            insert_into_sink(self.t_env, table, sink, ("id",))
